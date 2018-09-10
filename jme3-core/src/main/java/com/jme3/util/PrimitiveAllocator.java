cd3c3e035a52169cc1cf3c2844c6d514b4992a9b
Agung Udijana <agung.udijana@gmail.com>
2018-09-10 21:43:47 +0200

    uncommit test

--- jme3-core/src/main/java/com/jme3/util/PrimitiveAllocator.java
+++ jme3-core/src/main/java/com/jme3/util/PrimitiveAllocator.java
@@ -29,7 +29,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-//testing github
+
 package com.jme3.util;
 
 import java.nio.Buffer;
